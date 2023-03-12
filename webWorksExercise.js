88e42460e2a655bba3f1b64fe8859ecbe8ab4e1a
Issaac Pettengill <issaac@Issaacs-iMac.lan>
2023-03-11 22:04:09 -0500

    final drafts

---
+++ webWorksExercise.js
@@ -0,0 +1,8 @@
+// Part II
+
+// I was able to set up python3 and set up a simple web server
+
+// Part III
+
+// I do not see the field in the query string.  This information is viewed
+//under the dev tools using the network tab
